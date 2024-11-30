442dc14a050487da28b400b6c28fc82d4c36ef0d
joswellahwasike <joswellahwasike@gmail.com>
2024-11-30 15:30:46 +0300

    initial commit

--- custom-admin-panel/index.js
+++ custom-admin-panel/index.js
@@ -1,10 +1,45 @@
+// Importing configurations from different files
+import config from './config';
+import theme from './theme';
+import navigation from './navigation';
+
 export default {
-    config: {
-      locales: ['en', 'fr', 'es'], // Add support for multiple languages
+  config: {
+    ...config,         // Spread the content of 'config'
+    ...theme,          // Spread the content of 'theme'
+    ...navigation,     // Spread the content of 'navigation'
+    locales: ['en', 'fr', 'es'], // Adding support for multiple languages
+    wysiwyg: {
+      enabled: true,
+      customWysiwyg: {
+        component: '@tinymce/tinymce-react',
+        props: {
+          apiKey: 'your-tinymce-api-key',
+          init: {
+            plugins: 'link image code',
+            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code',
+          },
+        },
+      },
     },
-    bootstrap(app) {
-      // Custom logic when the admin panel is loaded
-      console.log('Custom admin panel loaded!');
+    navigation: [
+      {
+        title: 'Reports',
+        icon: 'chart-bar',
+        link: '/plugins/reports',
+      },
+    ],
+  },
+  theme: {
+    colors: {
+      primary100: '#D3E4CD',
+      primary200: '#ADC2A9',
+      primary500: '#6B705C',
+      neutral100: '#F5F5F5',
+      neutral500: '#333333',
     },
-  };
-  
+  },
+  bootstrap(app) {
+    console.log('Admin panel customization loaded successfully.');
+  },
+};
